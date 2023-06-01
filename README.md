12226d0a3d4b48e1da562cc7bb46c4afba2e7427
Luciano Angeleri <lcnang@gmail.com>
2023-06-01 16:18:32 -0300

    Se agregó la estructura principal del proyecto y el Readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Trabajo Practico N°10
---
+++ src/components/FormularioPeliculas.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormularioPeliculas = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FormularioPeliculas;
