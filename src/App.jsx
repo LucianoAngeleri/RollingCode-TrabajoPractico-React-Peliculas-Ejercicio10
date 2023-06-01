eb28de6b395442e7b2858a408e2be16c49f487df
Luciano Angeleri <lcnang@gmail.com>
2023-06-01 17:40:27 -0300

    Se agregó el maquetado del componente del formulario para cargar películas, junto con parte del funcionamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import FormularioPeliculas from "./components/FormularioPeliculas";
 
 function App() {
   return (
     <>
+      <Container className="py-4">
+        <h1 className="display-1 fw-bold">Ejercicio N°10</h1>
+        <h2 className="display-2">Administrador de Películas</h2>
+      </Container>
+      <Container className="py-4">
+        <FormularioPeliculas></FormularioPeliculas>
+      </Container>
+      <Container className="py-4">
+        <h2 className="display-2">Películas disponibles</h2>
+        <hr />
+      </Container>
+      <Container fluid className="text-center text-bg-dark py-5 mt-5">
+        <footer>
+          <p>&copy; Todos los derechos reservados.</p>
+        </footer>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FormularioPeliculas.jsx
+++ src/components/FormularioPeliculas.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import React from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+
+class Pelicula{
+  constructor(nombre, descripcion, genero) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.genero = genero;
+  }
+}
 
 const FormularioPeliculas = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [genero, setGenero] = useState("");
+  const [peliculas, setPeliculas] = useState([]);
+
+  useEffect(() => {
+    console.log(peliculas);
+  }, [peliculas]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const pelicula = new Pelicula(nombre, descripcion, genero);
+    setPeliculas([...peliculas, pelicula]);
+    setNombre("")
+    setDescripcion("")
+    setGenero("")
+  };
+  return (
+    <Row className="g-3 px-3 pb-5 px-md-5 rounded-5 border border-1 shadow-lg text-bg-secondary">
+      <Col>
+      <h3 className="display-3">Cargar una Película</h3>
+      <hr />
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-4" controlId="inputNombre">
+            <Form.Label>Nombre de Película</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Escribe el nombre de la película..."
+              onChange={(e) => setNombre(e.target.value)}
+              value={nombre}></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-4" controlId="inputDescripcion">
+                <Form.Label>Descripción de la Película</Form.Label>
+                <Form.Control as="textarea" rows={3} placeholder="Escriba una descripción sobre la película..." onChange={(e) => setDescripcion(e.target.value)}
+              value={descripcion}/>
+            </Form.Group>
+          <Form.Group className="mb-4" controlId="inputGenero">
+            <Form.Label>Género de la Película</Form.Label>
+            <Form.Select onChange={(e) => setGenero(e.target.value)} value={genero}>
+                <option>Selecciona un género</option>
+                <option value="accion">Acción</option>
+                <option value="drama">Drama</option>
+                <option value="aventura">Aventura</option>
+                <option value="terror">Terror</option>
+                <option value="ciencia ficcion">Ciencia Ficción</option>
+            </Form.Select>
+          </Form.Group>
+          <Col className="d-flex justify-content-center">
+            <Button variant="primary" type="submit" className="rounded-pill shadow py-2 px-3 py-md-3 px-md-4 fs-4">Guardar Película</Button>
+          </Col>
+        </Form>
+      </Col>
+    </Row>
+  );
 };
 
 export default FormularioPeliculas;
